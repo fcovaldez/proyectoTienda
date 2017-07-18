f1109a2a9c34d7ad35c7046460de2d0eadab7853
Alex-PC <alexx7valenzuela@hotmail.com>
2017-07-17 18:35:27 -0600

    creacion de ruta para guardar categorias

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/registrarcategorias', 'categoriascontroller@registrar');
+
+Route::post('/guardarcategorias','categoriasontroller@guardar');
+
