08cedbb5c8e9eef8b00a6cea8ab951ea950bd0a2
Alex-PC <alexx7valenzuela@hotmail.com>
2017-07-17 18:18:04 -0600

    creacion del metodo de registro de categorias

--- app/Http/Controllers/categoriascontroller.php
+++ app/Http/Controllers/categoriascontroller.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Categorias;
 
 class categoriascontroller extends Controller
 {
-    //
+    public function registrar(){
+    	$categorias=Categorias::all();
+    	return view('registrarCategorias', compact('categorias'));
+    }
 }
