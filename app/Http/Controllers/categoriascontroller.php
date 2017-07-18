a7143f462f0df16d51dcb8a30ed28f18665258c2
Alex-PC <alexx7valenzuela@hotmail.com>
2017-07-17 18:20:43 -0600

    creacion del metodo guardar de categorias

--- app/Http/Controllers/categoriascontroller.php
+++ app/Http/Controllers/categoriascontroller.php
@@ -12,4 +12,13 @@ class categoriascontroller extends Controller
     	$categorias=Categorias::all();
     	return view('registrarCategorias', compact('categorias'));
     }
+
+    public function guardar(Request $datos){
+    	$categorias= new Categorias(); //objeto del modelo encargado para registrar encargados
+    	$categorias->nombre=$datos->input('nombre');
+    	$categorias->descripcion=$datos->input('descripcion');
+    	$categorias->save();
+        flash('¡Categoria guardada con éxito!')->success();
+
+    	return redirect('/');
 }
