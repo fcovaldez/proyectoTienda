e8324b7b76ce289cf17a1a56f9fa214f45e6b8d1
Kevin <kevin_castro33@hotmail.com>
2017-07-18 22:48:52 -0600

    funcion guardar en articuloscontroller

--- app/Http/Controllers/articuloscontroller.php
+++ app/Http/Controllers/articuloscontroller.php
@@ -12,4 +12,14 @@ class articuloscontroller extends Controller
     	$articulos=Articulos::all();
     	return view('registrarArticulos', compact('articulos'));
     }
+
+    public function guardar(Request $datos){
+    	$articulos= new Articulos(); //objeto del modelo encargado para registrar encargados
+    	$articulos->nombre=$datos->input('nombre');
+    	$articulos->descripcion=$datos->input('descripcion');
+    	$articulos->save();
+        flash('¡Articulo guardado con éxito!')->success();
+
+    	return redirect('/');
+	}
 }
