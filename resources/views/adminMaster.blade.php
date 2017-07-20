ad0c3b7523ae62757217aa1cc7df315009bfa5dc
Kevin <kevin_castro33@hotmail.com>
2017-07-20 11:13:49 -0600

    anadio enlace a vista registrarCategorias

--- resources/views/adminMaster.blade.php
+++ resources/views/adminMaster.blade.php
@@ -34,7 +34,7 @@
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Categorias<span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Registrar Categoria</a></li>
+            <li><a href="{{url('/registrarcategorias')}}">Registrar Categoria</a></li>
             <li><a href="#">Consultar Categorias</a></li>
           </ul>
         </li>
