91412474018d54b371ba7ace1f7a114bded5ce0e
fcovaldez <fcolarranaga@gmail.com>
2017-07-19 23:04:44 -0600

    Creacion vista base para la pagina de Inicio

--- resources/views/userMaster.blade.php
+++ resources/views/userMaster.blade.php
@@ -16,7 +16,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">Tienda</a>
+      <a class="navbar-brand" href="{{url('/')}}">Tienda</a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
